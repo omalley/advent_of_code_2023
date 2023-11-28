4cb93ace2879c081ec4d12f05625f37a1795823c
Owen O'Malley <omalley@apache.org>
2023-11-28 14:50:12 -0800

    Support multiple days in benchmark target.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,28 +1,37 @@
 use omalley_aoc2023 as aoc_lib;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-macro_rules! benchmarks {
-  ($day:ident) => {
+macro_rules! benchmarks_internal {
+  ( $($day:ident),* ) => {
+    paste::paste!{
+      $(fn [<$day _benchmark>](c: &mut Criterion) {
+          use aoc_lib::$day;
+          let input_data = aoc_lib::utils::read_inputs("input", &vec![stringify!($day)])
+            .expect("can't read input");
+          let input = $day::generator(&input_data[0]);
+          c.bench_function(concat!(stringify!($day), " gen"), |b| {
+            b.iter(|| $day::generator(&input_data[0]))
+          });
+          c.bench_function(concat!(stringify!($day), " part 1"), |b| {
+            b.iter(|| $day::part1(&input))
+          });
+          c.bench_function(concat!(stringify!($day), " part 2"), |b| {
+            b.iter(|| $day::part2(&input))
+          });
+        }
+        criterion_group!($day, [<$day _benchmark>]);
+      )*
 
-    fn benchmark_function(c: &mut Criterion) {
-      use aoc_lib::$day;
-      let input_data = aoc_lib::utils::read_inputs("input", &vec![stringify!($day)])
-        .expect("can't read input");
-      let input = $day::generator(&input_data[0]);
-      c.bench_function(concat!(stringify!($day), " gen"), |b| {
-        b.iter(|| $day::generator(&input_data[0]))
-      });
-      c.bench_function(concat!(stringify!($day), " part 1"), |b| {
-        b.iter(|| $day::part1(&input))
-      });
-      c.bench_function(concat!(stringify!($day), " part 2"), |b| {
-        b.iter(|| $day::part2(&input))
-      });
+      criterion_main!($($day),*);
     }
   };
 }
 
-benchmarks!(day1);
+#[macro_export]
+macro_rules! benchmarks {
+  ( $($day:literal),* ) => {
+    paste::paste!{ benchmarks_internal!{$( [<day $day>] ),*} }
+  }
+}
 
-criterion_group!(benches, benchmark_function);
-criterion_main!(benches);
+benchmarks!(1);
