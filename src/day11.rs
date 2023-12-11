7ebe7dd5cf36db933c0d47ebbcea422b2f47b7d9
Owen O'Malley <omalley@apache.org>
2023-12-11 14:29:03 -0800

    Move work to generator

--- src/day11.rs
+++ src/day11.rs
@@ -1,20 +1,37 @@
+#[derive(Clone,Copy,Debug,Default)]
+pub struct StretchyCoordinates {
+  real: usize,
+  stretch: usize,
+}
+
+impl StretchyCoordinates {
+  fn abs_diff(&self, other: StretchyCoordinates) -> StretchyCoordinates {
+    StretchyCoordinates{real: self.real.abs_diff(other.real),
+      stretch: self.stretch.abs_diff(other.stretch)}
+  }
+
+  fn plus(&self, other: StretchyCoordinates) -> StretchyCoordinates {
+    StretchyCoordinates{real: self.real + other.real,
+      stretch: self.stretch + other.stretch}
+  }
+}
+
 #[derive(Clone,Debug)]
 pub struct Galaxy {
-  x: usize,
-  y: usize,
+  x: StretchyCoordinates,
+  y: StretchyCoordinates,
 }
 
 impl Galaxy {
-  fn distance(&self, other: &Galaxy) -> usize {
-    self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
+  fn distance(&self, other: &Galaxy) -> StretchyCoordinates {
+    self.x.abs_diff(other.x).plus(
+      self.y.abs_diff(other.y))
   }
 }
 
 #[derive(Clone,Debug)]
 pub struct Map {
   galaxies: Vec<Galaxy>,
-  expanding_rows: Vec<usize>,
-  expanding_columns: Vec<usize>,
 }
 
 impl Map {
@@ -32,7 +49,8 @@ impl Map {
       let mut is_empty = true;
       for (x, ch) in row.chars().enumerate() {
         if ch == '#' {
-          galaxies.push(Galaxy{x,y});
+          galaxies.push(Galaxy{x: StretchyCoordinates{real:x, stretch: 0},
+            y: StretchyCoordinates{real:y, stretch: 0}});
           is_empty = false;
           empty_columns[x] = false;
         }
@@ -41,25 +59,22 @@ impl Map {
         expanding_rows.push(y);
       }
     }
-    let expanding_columns = empty_columns.iter().enumerate()
+    let expanding_columns: Vec<usize> = empty_columns.iter().enumerate()
         .filter_map(|(i, &empty)| if empty { Some(i) } else { None })
         .collect();
-    Map{galaxies, expanding_rows, expanding_columns}
-  }
-
-  fn expand(&mut self, factor: usize) {
-    for g in self.galaxies.iter_mut() {
-      g.x += (factor - 1) * self.expanding_columns.partition_point(|col| *col < g.x);
-      g.y += (factor - 1) * self.expanding_rows.partition_point(|row| *row < g.y);
+    for g in galaxies.iter_mut() {
+      g.x.stretch = expanding_columns.partition_point(|col| *col < g.x.real);
+      g.y.stretch = expanding_rows.partition_point(|row| *row < g.y.real);
     }
+    Map{galaxies}
   }
 
-  fn sum_distances(&self) -> usize {
-    let mut sum = 0;
+  fn sum_distances(&self) -> StretchyCoordinates {
+    let mut sum = StretchyCoordinates::default();
     for (i, g1) in self.galaxies.iter().enumerate() {
       for (j, g2) in self.galaxies.iter().enumerate() {
         if i < j {
-          sum += g1.distance(g2);
+          sum = sum.plus(g1.distance(g2));
         }
       }
     }
@@ -67,20 +82,16 @@ impl Map {
   }
 }
 
-pub fn generator(input: &str) -> Map {
-  Map::from_str(input)
+pub fn generator(input: &str) -> StretchyCoordinates {
+  Map::from_str(input).sum_distances()
 }
 
-pub fn part1(input: &Map) -> usize {
-  let mut expanding_galaxy = input.clone();
-  expanding_galaxy.expand(2);
-  expanding_galaxy.sum_distances()
+pub fn part1(input: &StretchyCoordinates) -> usize {
+  input.real + input.stretch
 }
 
-pub fn part2(input: &Map) -> usize {
-  let mut expanding_galaxy = input.clone();
-  expanding_galaxy.expand(1_000_000);
-  expanding_galaxy.sum_distances()
+pub fn part2(input: &StretchyCoordinates) -> usize {
+  input.real + input.stretch * 999_999
 }
 
 #[cfg(test)]
@@ -106,8 +117,7 @@ mod tests {
 
   #[test]
   fn test_part2() {
-    let mut input = generator(INPUT);
-    input.expand(100);
-    assert_eq!(8410, input.sum_distances());
+    let input = generator(INPUT);
+    assert_eq!(8410, input.real + input.stretch * 99);
   }
 }
