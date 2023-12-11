b0d4fda42186ff674611f71fcfff55158bc3313b
Owen O'Malley <omalley@apache.org>
2023-12-10 22:31:55 -0800

    day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -47,39 +47,45 @@ impl Map {
     Map{galaxies, expanding_rows, expanding_columns}
   }
 
-  fn expand(&mut self) {
+  fn expand(&mut self, factor: usize) {
     for g in self.galaxies.iter_mut() {
-      g.x += self.expanding_columns.partition_point(|col| *col < g.x);
-      g.y += self.expanding_rows.partition_point(|row| *row < g.y);
+      g.x += (factor - 1) * self.expanding_columns.partition_point(|col| *col < g.x);
+      g.y += (factor - 1) * self.expanding_rows.partition_point(|row| *row < g.y);
     }
+  }
+
+  fn sum_distances(&self) -> usize {
+    let mut sum = 0;
+    for (i, g1) in self.galaxies.iter().enumerate() {
+      for (j, g2) in self.galaxies.iter().enumerate() {
+        if i < j {
+          sum += g1.distance(g2);
+        }
+      }
+    }
+    sum
   }
 }
 
 pub fn generator(input: &str) -> Map {
-  let mut result = Map::from_str(input);
-  result.expand();
-  result
+  Map::from_str(input)
 }
 
 pub fn part1(input: &Map) -> usize {
-  let mut sum = 0;
-  for (i, g1) in input.galaxies.iter().enumerate() {
-    for (j, g2) in input.galaxies.iter().enumerate() {
-      if i < j {
-        sum += g1.distance(g2);
-      }
-    }
-  }
-  sum
+  let mut expanding_galaxy = input.clone();
+  expanding_galaxy.expand(2);
+  expanding_galaxy.sum_distances()
 }
 
 pub fn part2(input: &Map) -> usize {
-  0
+  let mut expanding_galaxy = input.clone();
+  expanding_galaxy.expand(1_000_000);
+  expanding_galaxy.sum_distances()
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::day11::{generator, part1, part2};
+  use crate::day11::{generator, part1};
 
   const INPUT: &str =
 "...#......
@@ -100,6 +106,8 @@ mod tests {
 
   #[test]
   fn test_part2() {
-    assert_eq!(0, part2(&generator(INPUT)));
+    let mut input = generator(INPUT);
+    input.expand(100);
+    assert_eq!(8410, input.sum_distances());
   }
 }
