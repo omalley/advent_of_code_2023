4ccda70f7c58414f8508295dd022d4234d74c5e8
Owen O'Malley <omalley@apache.org>
2023-12-19 20:53:00 -0800

    day 19 part 2

--- src/day19.rs
+++ src/day19.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
+use std::fmt::Display;
+use std::ops::Range;
 
-type Rating = i32;
+type Rating = i16;
 type RuleId = u32;
 
 #[derive(Clone,Copy,Debug)]
@@ -93,6 +95,20 @@ impl Operator {
       Operator::Greater(target) => val > *target,
     }
   }
+
+  fn range(&self) -> Range<Rating> {
+    match self {
+      Operator::Less(lit) => Rating::MIN..*lit,
+      Operator::Greater(lit) => lit+1..Rating::MAX,
+    }
+  }
+
+  fn inverse_range(&self) -> Range<Rating> {
+    match self {
+      Operator::Less(lit) => *lit..Rating::MAX,
+      Operator::Greater(lit) => Rating::MIN..lit+1,
+    }
+  }
 }
 
 #[derive(Clone,Debug)]
@@ -123,6 +139,7 @@ impl RuleCondition {
 
 #[derive(Clone,Debug)]
 pub struct Rule {
+  #[allow(dead_code)]
   name: String,
   conditions: Vec<RuleCondition>,
   last: Target,
@@ -161,7 +178,7 @@ pub struct Input {
 }
 
 impl Input {
-  pub fn from_str(s: &str) -> Result<Self,String> {
+  fn from_str(s: &str) -> Result<Self,String> {
     let (rule_str, part_str) = s.split_once("\n\n")
         .ok_or("can't find part lists")?;
     let names = rule_str.lines().enumerate()
@@ -172,7 +189,7 @@ impl Input {
         .collect::<Result<HashMap<String, RuleId>,String>>()?;
     let rule_set = rule_str.lines().map(|s| Rule::from_str(s, &names))
         .collect::<Result<Vec<Rule>,String>>()?;
-    let parts = part_str.lines().map(|l| Part::from_str(l))
+    let parts = part_str.lines().map(Part::from_str)
         .collect::<Result<Vec<Part>,String>>()?;
     Ok(Input{rule_set, parts,
       start_rule: *names.get("in").ok_or("Can't find in rule".to_string())?})
@@ -190,6 +207,134 @@ impl Input {
   }
 }
 
+const ATTRIBUTE_RANGE: Range<Rating> = 1..4001;
+
+#[derive(Clone,Debug)]
+struct SymbolicValue {
+  ranges: Vec<Range<Rating>>,
+}
+
+impl SymbolicValue {
+  fn default() -> Self {
+    SymbolicValue{ranges: vec![ATTRIBUTE_RANGE]}
+  }
+
+  fn count(&self) -> u64 {
+    self.ranges.iter().map(|r| r.len() as u64).sum()
+  }
+
+  /// Restrict the current range by the new range.
+  fn and(&self, range: &Range<Rating>) -> Self {
+    let mut ranges = Vec::new();
+    for old_range in &self.ranges {
+      if range.end > old_range.start {
+        ranges.push(range.start.max(old_range.start)..range.end.min(old_range.end))
+      } else if old_range.end > range.start {
+        ranges.push(range.start..old_range.end);
+      }
+    }
+    ranges.retain(|r| !r.is_empty());
+    SymbolicValue{ranges}
+  }
+
+  fn is_empty(&self) -> bool {
+    self.ranges.is_empty()
+  }
+}
+
+#[derive(Clone,Debug)]
+struct SymbolicAttributes {
+  attributes: [SymbolicValue; NUM_ATTRIBUTES],
+}
+
+impl SymbolicAttributes {
+  fn default() -> Self {
+    SymbolicAttributes{attributes: [(); NUM_ATTRIBUTES].map(|_| SymbolicValue::default())}
+  }
+
+  fn is_empty(&self) -> bool {
+    self.attributes.iter().any(|v| v.is_empty())
+  }
+
+  fn count(&self) -> u64 {
+    self.attributes.iter().map(|a| a.count()).product()
+  }
+
+  fn update_attributes(&self, attribute: &AttributeId, range: &Range<Rating>) -> Self {
+    let mut new_values = self.clone();
+    let attribute_id = *attribute as usize;
+    new_values.attributes[attribute_id] = new_values.attributes[attribute_id].and(range);
+    new_values
+  }
+}
+
+impl Display for SymbolicAttributes {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "[X: {:?}, M: {:?} A: {:?} S: {:?}",self.attributes[0].ranges,
+           self.attributes[1].ranges, self.attributes[2].ranges, self.attributes[3].ranges)
+  }
+}
+
+#[derive(Clone,Debug)]
+struct State {
+  attributes: SymbolicAttributes,
+  rule: RuleId,
+  clause: RuleId,
+}
+
+impl State {
+  fn new(input: &Input) -> Self {
+    State{attributes: SymbolicAttributes::default(), rule: input.start_rule, clause: 0}
+  }
+
+  fn count(&self, input: &Input) -> u64 {
+    let mut pending = Vec::new();
+    let mut count = 0;
+    pending.push(self.clone());
+    while let Some(current) = pending.pop() {
+      if current.attributes.is_empty() {
+        continue;
+      }
+      let rule = &input.rule_set[current.rule as usize];
+      if (current.clause as usize) < rule.conditions.len() {
+        let condition = &rule.conditions[current.clause as usize];
+        let attributes = current.attributes.update_attributes(&condition.attribute,
+                                                              &condition.operator.inverse_range());
+        let next = State{attributes, rule: current.rule, clause: current.clause + 1};
+        pending.push(next);
+        let attributes = current.attributes.update_attributes(&condition.attribute,
+                                                              &condition.operator.range());
+        match condition.target {
+          Target::GoTo(target) => {
+            pending.push(
+              State{attributes, rule: target, clause: 0});
+          },
+          Target::Accept => count += attributes.count(),
+          Target::Reject => {}
+        }
+      } else {
+        match rule.last {
+          Target::GoTo(target) => {
+            let mut next = current.clone();
+            next.rule = target;
+            next.clause = 0;
+            pending.push(next);
+          }
+          Target::Accept => count += current.attributes.count(),
+          Target::Reject => { },
+        }
+      }
+    }
+    count
+  }
+}
+
+impl Display for State {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "rule: {}.{}, {}", self.rule, self.clause, self.attributes)
+  }
+}
+
 pub fn generator(input: &str) -> Input {
   Input::from_str(input).unwrap()
 }
@@ -198,13 +343,13 @@ pub fn part1(input: &Input) -> i64 {
   input.parts.iter().filter(|&p| input.accept(p)).map(|p| p.value()).sum()
 }
 
-pub fn part2(input: &Input) -> i64 {
-  0
+pub fn part2(input: &Input) -> u64 {
+  State::new(input).count(input)
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::day19::{generator, part1, part2};
+  use crate::day19::{generator, part1, part2, SymbolicValue};
 
   const INPUT: &str =
 "px{a<2006:qkq,m>2090:A,rfg}
@@ -231,7 +376,21 @@ hdj{m>838:A,pv}
   }
 
   #[test]
+  fn test_conditions() {
+    let test_value = SymbolicValue::default();
+    assert_eq!("SymbolicValue { ranges: [1000..4001] }",
+               format!("{:?}", test_value.and(&(1000..5000))));
+    assert_eq!("SymbolicValue { ranges: [1..123] }",
+               format!("{:?}", test_value.and(&(-1000..123))));
+    assert_eq!("SymbolicValue { ranges: [1..4001] }",
+               format!("{:?}", test_value.and(&(-1000..5000))));
+    assert_eq!("SymbolicValue { ranges: [100..500] }",
+               format!("{:?}", test_value.and(&(100..500))));
+    assert_eq!(true, test_value.and(&(4001..5000)).is_empty());
+  }
+
+  #[test]
   fn test_part2() {
-    assert_eq!(952408144115, part2(&generator(INPUT)));
+    assert_eq!(167409079868000, part2(&generator(INPUT)));
   }
 }
