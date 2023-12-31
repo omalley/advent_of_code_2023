3d093afb30ecfeaa39674a6a3bc929811ff67c8b
Owen O'Malley <omalley@apache.org>
2023-12-30 19:20:28 -0800

    printing out the grid times.

--- src/day21.rs
+++ src/day21.rs
@@ -1,8 +1,6 @@
-use std::cmp::Reverse;
 use std::collections::{HashMap, HashSet};
 use std::ops::Range;
-use priority_queue::PriorityQueue;
-use smallvec::{SmallVec, smallvec};
+use smallvec::SmallVec;
 
 #[derive(Clone,Debug,Eq,PartialEq)]
 pub enum Spot {
@@ -106,112 +104,47 @@ impl Map {
 
   fn moves<const LIMITLESS: bool>(&self, dist: Time) -> usize {
     let mut frontier : HashSet<Location> = HashSet::new();
-    let mut done = [(); 2].map(|_| HashSet::new());
+    let mut done = [(); 2].map(|_| HashMap::new());
+    let mut first = HashMap::new();
     frontier.insert(self.start.clone());
+    first.insert(GridOffset{x:0, y:0}, 0 as Time);
     for t in 0..dist {
       let mut next = HashSet::new();
       for loc in frontier.into_iter() {
         for n in self.next::<LIMITLESS>(loc) {
-          if done[t as usize % 2].insert(n.clone()) {
+          done[t as usize % 2].entry(n.clone()).or_insert_with(|| {
+            first.entry(GridOffset{x: n.x / self.width.end, y: n.y / self.height.end})
+                .or_insert_with(|| t);
             next.insert(n);
-          }
+          });
         }
       }
       frontier = next;
+    }
+    let mut y_grid = 0..1 as Coordinate;
+    let mut x_grid = 0..1 as Coordinate;
+    for g in first.keys() {
+      if !y_grid.contains(&g.y) {
+        y_grid.start = y_grid.start.min(g.y);
+        y_grid.end = y_grid.end.max(g.y + 1);
+      }
+      if !x_grid.contains(&g.x) {
+        x_grid.start = x_grid.start.min(g.x);
+        x_grid.end = x_grid.end.max(g.x + 1);
+      }
+    }
+    for y in y_grid.clone() {
+      for x in x_grid.clone() {
+        if let Some(t) = first.get(&GridOffset{x, y}) {
+          print!(" {t:>4}");
+        } else {
+          print!("    .");
+        }
+      }
+      println!();
     }
     //self.print_map(&done[(dist + 1) % 2]);
     done[(dist as usize + 1) % 2].len()
-  }
-
-  fn summarize_grid(&self, orig_entries: EntryList) -> GridSummary {
-    let mut entries = orig_entries.clone();
-    entries.sort_unstable();
-    let mut frontier : HashSet<Location> = HashSet::new();
-    let mut done = [(); 2].map(|_| HashSet::new());
-    let mut outside_done = HashSet::new();
-    let mut counts = Vec::new();
-    let mut exits: Vec<Exit> = Vec::new();
-    while !entries.is_empty() && entries[0].time == 0 {
-      let entry = entries.remove(0);
-      frontier.insert(entry.location.clone());
-      done[0].insert(entry.location);
-    }
-    counts.push(orig_entries.len() - entries.len());
-    for time in 1..Time::MAX {
-      while !entries.is_empty() && entries[0].time == time {
-        let entry = entries.remove(0);
-        frontier.insert(entry.location.clone());
-        done[time as usize % 2].insert(entry.location);
-      }
-      let mut next = HashSet::new();
-      for loc in frontier.into_iter() {
-        for n in self.next::<true>(loc) {
-          if !self.width.contains(&n.x) || !self.height.contains(&n.y) {
-            let grid= GridOffset{x: n.x.div_euclid(self.width.end),
-              y: n.y.div_euclid(self.height.end)};
-            if self.width.contains(&loc.x) && self.height.contains(&loc.y) {
-              let location = Location{x: n.x.rem_euclid(self.width.end),
-                y: n.y.rem_euclid(self.height.end)};
-              if let Some(e) = exits.iter_mut()
-                  .find(|&e| e.grid == grid.clone()) {
-                e.locations.push(EntrySpot{time: time - e.time, location})
-              } else {
-                exits.push(Exit{ time, grid,
-                  locations: smallvec![EntrySpot{time: 0, location}] })
-              }
-            }
-            if outside_done.insert(n.clone()) {
-              next.insert(n);
-            }
-          } else if done[time as usize % 2].insert(n.clone()) {
-            next.insert(n);
-          }
-        }
-      }
-      // Get the current number of spots marked
-      let current_size = done[time as usize % 2].len();
-      // If it is the same as 2 steps ago, we are done.
-      if counts.len() >= 2 && counts[counts.len() - 2] == current_size {
-        break;
-      }
-      counts.push(current_size);
-      frontier = next;
-    }
-    for e in exits.iter_mut() {
-      e.locations.sort_unstable();
-    }
-    GridSummary{counts, exits}
-  }
-
-  fn grid_move(&self, dist: Time) -> usize {
-    let mut pending: PriorityQueue<Exit, Reverse<usize>> = PriorityQueue::new();
-    let mut cache = HashMap::new();
-    let mut done = HashSet::new();
-    let mut count = 0;
-    pending.push(Exit{time: 0, grid: GridOffset{x: 0, y: 0}, location: self.start.clone()},
-                 Reverse(0));
-    while let Some((exit, _)) = pending.pop() {
-      if done.len() % 10_000 == 0 {
-        println!("t = {}, done = {}", exit.time, done.len());
-      }
-      //println!("popping {:?}, remaining = {}, cache = {}", exit, pending.len(), cache.len());
-      let summary = cache.entry(exit.location.clone())
-          .or_insert_with(|| self.summarize_grid(exit.location.clone()));
-      //println!("{:?}", summary);
-      let new_count = summary.count_squares(dist - exit.time);
-      count += new_count;
-      //println!("adding {} for {:?} at {}", new_count, exit.grid, exit.time);
-      done.insert(exit.grid.clone());
-      for next in &summary.exits {
-        let time = next.time + exit.time;
-        let grid = GridOffset{x: next.grid.x + exit.grid.x, y: next.grid.y + exit.grid.y};
-        if time < dist && !done.contains(&grid) {
-          pending.push_increase(Exit{time, grid, location: next.location.clone() },
-                                Reverse(time));
-        }
-      }
-    }
-    count
   }
 }
 
@@ -221,25 +154,10 @@ struct GridOffset {
   y: Coordinate,
 }
 
-#[derive(Clone,Debug,Eq,Hash,Ord,PartialEq,PartialOrd)]
-struct EntrySpot {
-  time: Time,
-  location: Location,
-}
-
-type EntryList = SmallVec<[EntrySpot; 2]>;
-
-#[derive(Clone,Debug,Eq,Hash,PartialEq)]
-struct Exit {
-  time: Time,
-  grid: GridOffset,
-  locations: EntryList,
-}
-
 #[derive(Clone,Debug)]
 struct GridSummary {
+  time: Time,
   counts: Vec<usize>,
-  exits: Vec<Exit>,
 }
 
 impl GridSummary {
@@ -264,13 +182,14 @@ pub fn part1(input: &Map) -> usize {
   input.moves::<false>(64)
 }
 
-pub fn part2(input: &Map) -> usize {
-  input.grid_move(26501365)
+pub fn part2(_input: &Map) -> usize {
+  //input.grid_move(26501365)
+  0
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::day21::{Exit, generator, GridOffset, Location};
+  use crate::day21::{generator};
 
   const INPUT: &str =
 "...........
@@ -294,18 +213,9 @@ mod tests {
   #[test]
   fn test_part2() {
     let input = generator(INPUT);
-    //assert_eq!(668697, input.moves::<true>(1000));
-    assert_eq!(50, input.moves::<true>(10));
-    assert_eq!(50, input.grid_move(10));
+    assert_eq!(668697, input.moves::<true>(160));
+    //assert_eq!(50, input.moves::<true>(10));
+    //assert_eq!(50, input.grid_move(10));
     //assert_eq!(668697, input.grid_move(1000));
-  }
-
-  #[test]
-  fn test_summary() {
-    let input = generator(INPUT);
-    let summary = input.summarize_grid(input.start.clone());
-    assert_eq!(16, summary.counts[6]);
-    assert_eq!(Exit{time:7, grid: GridOffset{x: -1, y: 0}, location: Location{x: 10, y: 4}},
-               summary.exits[0]);
   }
 }
